7f356948670aa93aefa0f2a463dddfab3a727109
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-23 19:04:21 +0530

    CSVtoCSV Opearation completed

--- CsvHelperAndJsonNET/Program.cs
+++ CsvHelperAndJsonNET/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,17 @@ namespace CsvHelperAndJsonNET
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to CSVHelper & JSON.NET Library.");
+            Console.WriteLine("\n1. CSV Helper\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CSVtoCSVOperation.ImplimentCSVHandling();
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    break;
+            }
             Console.ReadLine();
         }
     }
